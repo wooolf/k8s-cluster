59fce85d68d111f991289f3dd8663e282b04bc39
Michal LZ <awooolf@gmail.com>
2022-08-05 19:11:28 +0200

    Finished etcd config and deployment

---
+++ roles/prepare_k8s_etcd/files/deploy_etcd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+INTERNAL_IP=$(hostname -I | cut -d' ' -f1)
+ETCD_NAME=$(dig -x ${INTERNAL_IP} +noall +answer | grep PTR | cut -d$'\t' -f3)
+ETCD_NAME=$(echo ${ETCD_NAME::-1})
+
+cat <<EOF | sudo tee /etc/systemd/system/etcd.service
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos
+
+[Service]
+Type=notify
+ExecStart=/usr/local/bin/etcd \\
+  --name ${ETCD_NAME} \\
+  --cert-file=/etc/etcd/kubernetes.pem \\
+  --key-file=/etc/etcd/kubernetes-key.pem \\
+  --peer-cert-file=/etc/etcd/kubernetes.pem \\
+  --peer-key-file=/etc/etcd/kubernetes-key.pem \\
+  --trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://${ETCD_NAME}:2380 \\
+  --listen-peer-urls https://${INTERNAL_IP}:2380 \\
+  --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://${INTERNAL_IP}:2379 \\
+  --initial-cluster-token etcd-cluster-0 \\
+  --initial-cluster ${ETCD_NAME}=https://${INTERNAL_IP}:2380 \\
+  --initial-cluster-state new \\
+  --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
