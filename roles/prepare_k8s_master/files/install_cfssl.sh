25f9fd3ddddf3ab10fdf568cb6166041b4d56147
Michal LZ <awooolf@gmail.com>
2022-08-05 10:05:21 +0200

    Add existing project files to Git

---
+++ roles/prepare_k8s_master/files/install_cfssl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -q --show-progress --https-only --timestamping \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssl \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssljson
+chmod +x cfssl cfssljson
+sudo mv cfssl cfssljson /usr/local/bin/
+cfssl version
